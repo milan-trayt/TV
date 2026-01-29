227c8abf8770ff5dbf12e32fae0385fe31f8590e
milan-trayt <milan@trayt.health>
2026-01-29 21:06:36 +0545

    fix

--- backend/server.js
+++ backend/server.js
@@ -232,10 +232,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Content-Length']
 }))
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}))
 app.use(cookieParser())
 app.use(express.json())
 
